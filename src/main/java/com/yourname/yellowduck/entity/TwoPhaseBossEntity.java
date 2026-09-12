0ca57d75bbbd36e3d616e43f1340679ba6993e27
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-13 00:08:47 +0800

    Create TwoPhaseBossEntity.java

---
+++ src/main/java/com/yourname/yellowduck/entity/TwoPhaseBossEntity.java
@@ -0,0 +1,83 @@
+package com.yourname.yellowduck.entity;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.util.GeckoLibUtil;
+
+public class TwoPhaseBossEntity extends PathfinderMob implements GeoEntity {
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+
+    public static final EntityDataAccessor<Boolean> IS_PHASE_TWO =
+            SynchedEntityData.defineId(TwoPhaseBossEntity.class, EntityDataSerializers.BOOLEAN);
+
+    public TwoPhaseBossEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(IS_PHASE_TWO, false);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.level().isClientSide) {
+            if (this.getHealth() < this.getMaxHealth() * 0.5f && !this.entityData.get(IS_PHASE_TWO)) {
+                this.entityData.set(IS_PHASE_TWO, true);
+                this.level().playSound(null, this.blockPosition(), SoundEvents.WITHER_SPAWN, SoundSource.HOSTILE, 1.0F, 1.0F);
+                this.setHealth(this.getMaxHealth() * 0.75f);
+            }
+        }
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "controller", 5, state -> {
+            if (this.entityData.get(IS_PHASE_TWO)) {
+                return state.setAndContinue(RawAnimation.begin().thenLoop("animation.muscle_yellow_bird.idle"));
+            } else {
+                return state.setAndContinue(RawAnimation.begin().thenLoop("animation.yellow_bow_bird.idle"));
+            }
+        }));
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return this.cache;
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return PathfinderMob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 600.0D)
+                .add(Attributes.ATTACK_DAMAGE, 14.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.25D);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        super.addAdditionalSaveData(tag);
+        tag.putBoolean("IsPhaseTwo", this.entityData.get(IS_PHASE_TWO));
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        this.entityData.set(IS_PHASE_TWO, tag.getBoolean("IsPhaseTwo"));
+    }
+}
