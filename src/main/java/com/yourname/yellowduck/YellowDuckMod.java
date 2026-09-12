075ec6fd8a8fc018ca2fcc14df04c0b4ba6c4cce
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-12 23:59:44 +0800

    Create YellowDuckMod.java

---
+++ src/main/java/com/yourname/yellowduck/YellowDuckMod.java
@@ -0,0 +1,23 @@
+package com.yourname.yellowduck;
+
+import com.yourname.yellowduck.registry.ModEntities;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(YellowDuckMod.MOD_ID)
+public class YellowDuckMod {
+    public static final String MOD_ID = "yellowduck";
+
+    public YellowDuckMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModEntities.ENTITIES.register(bus);
+        bus.addListener(this::registerAttributes);
+    }
+
+    private void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.TWO_PHASE_BOSS.get(),
+                com.yourname.yellowduck.entity.TwoPhaseBossEntity.createAttributes().build());
+    }
+}
