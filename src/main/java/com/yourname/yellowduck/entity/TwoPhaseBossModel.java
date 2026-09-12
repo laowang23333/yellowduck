9cc79da45252a18772217a0e9261f8f08cd9535c
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-13 00:03:19 +0800

    Create TwoPhaseBossModel.java

---
+++ src/main/java/com/yourname/yellowduck/entity/TwoPhaseBossModel.java
@@ -0,0 +1,33 @@
+package com.yourname.yellowduck.entity;
+
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.GeoModel;
+
+public class TwoPhaseBossModel extends GeoModel<TwoPhaseBossEntity> {
+    @Override
+    public ResourceLocation getModelResource(TwoPhaseBossEntity animatable) {
+        if (animatable.entityData.get(TwoPhaseBossEntity.IS_PHASE_TWO)) {
+            return new ResourceLocation("yellowduck", "geo/muscle_yellow_bird.geo.json");
+        } else {
+            return new ResourceLocation("yellowduck", "geo/yellow_bow_bird.geo.json");
+        }
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(TwoPhaseBossEntity animatable) {
+        if (animatable.entityData.get(TwoPhaseBossEntity.IS_PHASE_TWO)) {
+            return new ResourceLocation("yellowduck", "textures/entity/muscle_yellow_bird.png");
+        } else {
+            return new ResourceLocation("yellowduck", "textures/entity/yellow_bow_bird.png");
+        }
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(TwoPhaseBossEntity animatable) {
+        if (animatable.entityData.get(TwoPhaseBossEntity.IS_PHASE_TWO)) {
+            return new ResourceLocation("yellowduck", "animations/muscle_yellow_bird.animation.json");
+        } else {
+            return new ResourceLocation("yellowduck", "animations/yellow_bow_bird.animation.json");
+        }
+    }
+}
