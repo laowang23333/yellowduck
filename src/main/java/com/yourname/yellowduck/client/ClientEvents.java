97a8c12add662a92831370ff973bd8be9d67f162
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-13 00:05:04 +0800

    Create ClientEvents.java

---
+++ src/main/java/com/yourname/yellowduck/client/ClientEvents.java
@@ -0,0 +1,16 @@
+package com.yourname.yellowduck.client;
+
+import com.yourname.yellowduck.YellowDuckMod;
+import com.yourname.yellowduck.registry.ModEntities;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = YellowDuckMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEvents {
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.TWO_PHASE_BOSS.get(), TwoPhaseBossRenderer::new);
+    }
+}
