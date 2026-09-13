f2675b7affdeaace1b564ca3f8e3b2e45197a73f
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 06:06:45 +0800

    Create ModMenuTypes.java

---
+++ src/main/java/com/yourname/yellowduck/registry/ModMenuTypes.java
@@ -0,0 +1,17 @@
+package com.yourname.yellowduck.registry;
+
+import com.yourname.yellowduck.YellowDuckMod;
+import com.yourname.yellowduck.menu.BigChestMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModMenuTypes {
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(ForgeRegistries.MENU_TYPES, YellowDuckMod.MODID);
+
+    public static final RegistryObject<MenuType<BigChestMenu>> BIG_CHEST =
+            MENUS.register("big_chest", () -> IForgeMenuType.create(BigChestMenu::new));
+}
