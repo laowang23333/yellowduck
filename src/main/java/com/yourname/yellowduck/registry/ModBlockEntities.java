d44463f9a36fa10dcb2f37bec4ac14a54b2776ce
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 06:06:02 +0800

    Create ModBlockEntities.java

---
+++ src/main/java/com/yourname/yellowduck/registry/ModBlockEntities.java
@@ -0,0 +1,18 @@
+package com.yourname.yellowduck.registry;
+
+import com.yourname.yellowduck.YellowDuckMod;
+import com.yourname.yellowduck.block.BigChestBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockEntities {
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
+            DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, YellowDuckMod.MODID);
+
+    public static final RegistryObject<BlockEntityType<BigChestBlockEntity>> BIG_CHEST =
+            BLOCK_ENTITIES.register("big_chest",
+                    () -> BlockEntityType.Builder.of(BigChestBlockEntity::new,
+                            ModBlocks.BIG_CHEST.get()).build(null));
+}
