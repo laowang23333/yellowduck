91c8d4abd0fad8ec6111a82405e38bf97d8aa69a
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 07:12:36 +0800

    Update ModBlocks.java

--- src/main/java/com/yourname/yellowduck/registry/ModBlocks.java
+++ src/main/java/com/yourname/yellowduck/registry/ModBlocks.java
@@ -2,6 +2,8 @@ package com.yourname.yellowduck.registry;
 
 import com.yourname.yellowduck.YellowDuckMod;
 import com.yourname.yellowduck.block.BigChestBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
@@ -13,7 +15,13 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, YellowDuckMod.MOD_ID);
 
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, YellowDuckMod.MOD_ID);
+
     public static final RegistryObject<Block> BIG_CHEST = BLOCKS.register("big_chest",
             () -> new BigChestBlock(BlockBehaviour.Properties.of()
                     .mapColor(MapColor.WOOD).strength(2.5F).noOcclusion()));
+
+    public static final RegistryObject<Item> BIG_CHEST_ITEM = ITEMS.register("big_chest",
+            () -> new BlockItem(BIG_CHEST.get(), new Item.Properties()));
 }
