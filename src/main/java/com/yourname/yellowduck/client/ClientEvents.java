7282ce429d83a5ee01ecb4e718bd0e0c8bd9e275
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 06:40:37 +0800

    Update ClientEvents.java

--- src/main/java/com/yourname/yellowduck/client/ClientEvents.java
+++ src/main/java/com/yourname/yellowduck/client/ClientEvents.java
@@ -3,25 +3,31 @@ package com.yourname.yellowduck.client;
 import com.yourname.yellowduck.registry.ModBlockEntities;
 import com.yourname.yellowduck.registry.ModEntities;
 import com.yourname.yellowduck.registry.ModMenuTypes;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterMenuScreensEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = "yellowduck", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
 
     @SubscribeEvent
     public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        // 坐骑
         MountRenderer.register(event, ModEntities.MOUNT.get());
+        // 大箱子方块实体
         event.registerBlockEntityRenderer(
                 ModBlockEntities.BIG_CHEST.get(),
                 BigChestRenderer::new);
     }
 
     @SubscribeEvent
-    public static void onRegisterMenuScreens(RegisterMenuScreensEvent event) {
-        event.register(ModMenuTypes.BIG_CHEST.get(), BigChestScreen::new);
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            // 大箱子 GUI
+            MenuScreens.register(ModMenuTypes.BIG_CHEST.get(), BigChestScreen::new);
+        });
     }
 }
