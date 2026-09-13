cab9fcae7a40541436eef402ea93b61c1e174461
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 01:00:44 +0800

    Update MountEntity.java

--- src/main/java/com/yourname/yellowduck/entity/MountEntity.java
+++ src/main/java/com/yourname/yellowduck/entity/MountEntity.java
@@ -23,7 +23,6 @@ public class MountEntity extends PathfinderMob {
                 .add(Attributes.FOLLOW_RANGE, 35.0D);
     }
 
-    // 右键骑上去
     @Override
     public InteractionResult mobInteract(Player player, InteractionHand hand) {
         if (!this.level().isClientSide && !this.isVehicle()) {
@@ -32,34 +31,26 @@ public class MountEntity extends PathfinderMob {
         return InteractionResult.sidedSuccess(this.level().isClientSide);
     }
 
-    // 玩家骑乘时，用 WASD 控制方向
     @Override
     public void travel(Vec3 travelVector) {
         if (this.isVehicle() && this.getControllingPassenger() instanceof Player player) {
-            // 跟随玩家视角
             this.setYRot(player.getYRot());
             this.yRotO = this.getYRot();
             this.setXRot(player.getXRot() * 0.5F);
             this.setRot(this.getYRot(), this.getXRot());
             this.yHeadRot = this.yBodyRot = this.getYRot();
 
-            // 读取玩家 WASD 输入
             float strafe = player.xxa * 0.5F;
             float forward = player.zza;
             if (forward <= 0.0F) forward *= 0.25F;
 
-            // 设置移动速度
             this.setSpeed((float) this.getAttributeValue(Attributes.MOVEMENT_SPEED));
-
-            // 应用移动
             super.travel(new Vec3(strafe, travelVector.y, forward));
         } else {
-            // 无骑乘者时正常 AI 移动
             super.travel(travelVector);
         }
     }
 
-    // 让骑乘者跟随旋转
     @Override
     public boolean isControlledByLocalInstance() {
         return this.isVehicle() && this.getControllingPassenger() instanceof Player;
