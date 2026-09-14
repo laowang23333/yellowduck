d97ee543f23f50f89dcc466750eba43193e26339
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 16:38:16 +0800

    Create MeetStoneBlock.java

---
+++ src/main/java/com/yourname/yellowduck/block/MeetStoneBlock.java
@@ -0,0 +1,31 @@
+package com.yourname.yellowduck.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class MeetStoneBlock extends Block implements EntityBlock {
+    // 0.0 到 32.0 代表占满上下两格
+    private static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 32.0D, 16.0D);
+
+    public MeetStoneBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new MeetStoneBlockEntity(pos, state);
+    }
+}
