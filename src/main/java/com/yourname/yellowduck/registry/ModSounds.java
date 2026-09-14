8b25457ac350f484d4d1d327c23dc4b2f8e8fb84
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 19:19:12 +0800

    Create ModSounds.java

---
+++ src/main/java/com/yourname/yellowduck/registry/ModSounds.java
@@ -0,0 +1,21 @@
+package com.yourname.yellowduck.registry;
+
+import com.yourname.yellowduck.YellowDuckMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModSounds {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, YellowDuckMod.MOD_ID);
+
+    public static final RegistryObject<SoundEvent> JIJIJI = register("jijiji");
+    public static final RegistryObject<SoundEvent> NIGANMA = register("niganma");
+    public static final RegistryObject<SoundEvent> ROUDAN = register("roudan");
+
+    private static RegistryObject<SoundEvent> register(String name) {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(YellowDuckMod.MOD_ID, name)));
+    }
+}
