89565002357664c018c527854b038a3787d9e687
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-15 01:44:25 +0800

    Update SakurawitchEntity.java

--- src/main/java/com/yourname/yellowduck/entity/SakurawitchEntity.java
+++ src/main/java/com/yourname/yellowduck/entity/SakurawitchEntity.java
@@ -22,6 +22,7 @@ import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -77,7 +78,7 @@ public class SakurawitchEntity extends PathfinderMob {
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 16.0F));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(2, new HateTargetGoal(this, 32.0));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     @Override
