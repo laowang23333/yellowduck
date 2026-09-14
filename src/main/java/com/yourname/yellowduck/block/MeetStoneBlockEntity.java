e549820d02ea19b5139629663a9bbb58b1436645
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-14 16:58:16 +0800

    Update MeetStoneBlockEntity.java

--- src/main/java/com/yourname/yellowduck/block/MeetStoneBlockEntity.java
+++ src/main/java/com/yourname/yellowduck/block/MeetStoneBlockEntity.java
@@ -3,10 +3,12 @@ package com.yourname.yellowduck.block;
 import com.yourname.yellowduck.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class MeetStoneBlockEntity extends BlockEntity {
-    public MeetStoneBlockEntity(BlockPos pos, BlockState state) {
-        super(ModBlockEntities.MEET_STONE.get(), pos, state);
+    // 加上 BlockEntityType 参数，并传递给 super
+    public MeetStoneBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
     }
 }
