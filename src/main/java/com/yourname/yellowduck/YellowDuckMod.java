ddf25a52e1c93bd458be6fb0e3d5335e02436e1d
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-15 11:07:10 +0800

    Update YellowDuckMod.java

--- src/main/java/com/yourname/yellowduck/YellowDuckMod.java
+++ src/main/java/com/yourname/yellowduck/YellowDuckMod.java
@@ -6,6 +6,7 @@ import com.yourname.yellowduck.entity.TwoPhaseBossEntity;
 import com.yourname.yellowduck.registry.ModEffects;
 import com.yourname.yellowduck.registry.ModEntities;
 import com.yourname.yellowduck.registry.ModSounds;
+import com.yourname.yellowduck.particle.ModParticles;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -23,6 +24,7 @@ public class YellowDuckMod {
         com.yourname.yellowduck.registry.ModMenuTypes.MENUS.register(bus);
         com.yourname.yellowduck.registry.ModCreativeTabs.CREATIVE_TABS.register(bus);
         ModEntities.ENTITIES.register(bus);
+        ModParticles.PARTICLES.register(bus);
 
         // 注册音效
         ModSounds.SOUND_EVENTS.register(bus);
