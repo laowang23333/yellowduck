6a33580d639d6230f4437b010056d64124325232
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-15 13:17:28 +0800

    Update ModSounds.java

--- src/main/java/com/yourname/yellowduck/registry/ModSounds.java
+++ src/main/java/com/yourname/yellowduck/registry/ModSounds.java
@@ -8,14 +8,26 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModSounds {
+
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, YellowDuckMod.MOD_ID);
 
+    // 鸡哥音效
     public static final RegistryObject<SoundEvent> JIJIJI = register("jijiji");
     public static final RegistryObject<SoundEvent> NIGANMA = register("niganma");
     public static final RegistryObject<SoundEvent> ROUDAN = register("roudan");
 
+    // 小樱音效
+    public static final RegistryObject<SoundEvent> SAKURA_ATT = register("sakura_att");
+    public static final RegistryObject<SoundEvent> SAKURA_END = register("sakura_End");
+    public static final RegistryObject<SoundEvent> SAKURA_XIONG = register("sakura_xiong");
+
     private static RegistryObject<SoundEvent> register(String name) {
-        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(YellowDuckMod.MOD_ID, name)));
+        return SOUND_EVENTS.register(
+                name,
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(YellowDuckMod.MOD_ID, name)
+                )
+        );
     }
 }
