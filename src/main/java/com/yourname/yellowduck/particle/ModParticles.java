9506481785ce4d26b1037883b29d533c8c106600
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-15 12:05:30 +0800

    Update ModParticles.java

--- src/main/java/com/yourname/yellowduck/particle/ModParticles.java
+++ src/main/java/com/yourname/yellowduck/particle/ModParticles.java
@@ -1,6 +1,7 @@
 package com.yourname.yellowduck.particle;
 
 import com.yourname.yellowduck.YellowDuckMod;
+import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,24 +10,33 @@ import net.minecraftforge.registries.RegistryObject;
 public final class ModParticles {
     private ModParticles() {}
 
-    public static final DeferredRegister<SimpleParticleType> PARTICLES =
-            DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, YellowDuckMod.MOD_ID);
+    public static final DeferredRegister<ParticleType<?>> PARTICLES =
+            DeferredRegister.create(
+                    ForgeRegistries.PARTICLE_TYPES,
+                    YellowDuckMod.MOD_ID
+            );
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_FLAME =
-            PARTICLES.register("sakura_flame", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_FLAME =
+            PARTICLES.register("sakura_flame",
+                    () -> new SimpleParticleType(true));
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_PETAL =
-            PARTICLES.register("sakura_petal", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_PETAL =
+            PARTICLES.register("sakura_petal",
+                    () -> new SimpleParticleType(true));
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_MAGIC =
-            PARTICLES.register("sakura_magic", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_MAGIC =
+            PARTICLES.register("sakura_magic",
+                    () -> new SimpleParticleType(true));
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_WARNING =
-            PARTICLES.register("sakura_warning", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_WARNING =
+            PARTICLES.register("sakura_warning",
+                    () -> new SimpleParticleType(true));
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_EXPLOSION =
-            PARTICLES.register("sakura_explosion", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_EXPLOSION =
+            PARTICLES.register("sakura_explosion",
+                    () -> new SimpleParticleType(true));
 
-    public static final RegistryObject<SimpleParticleType> SAKURA_ERUPTION =
-            PARTICLES.register("sakura_eruption", () -> new SimpleParticleType(true));
+    public static final RegistryObject<ParticleType<?>> SAKURA_ERUPTION =
+            PARTICLES.register("sakura_eruption",
+                    () -> new SimpleParticleType(true));
 }
