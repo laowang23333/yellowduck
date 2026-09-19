f5e393f1ccab32fb634f2280251df407d61c7cdc
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-19 16:43:16 +0800

    Update YellowDuckMod.java

--- src/main/java/com/yourname/yellowduck/YellowDuckMod.java
+++ src/main/java/com/yourname/yellowduck/YellowDuckMod.java
@@ -47,6 +47,7 @@ public class YellowDuckMod {
         event.put(ModEntities.MOUNT.get(), MountEntity.createAttributes().build());
         event.put(ModEntities.ALPACA_MOUNT.get(), AlpacaMountEntity.createAttributes().build());
         event.put(ModEntities.RABBIT_MOUNT.get(), MountEntity.createAttributes().build());
+        event.put(ModEntities.BAMBOO_HORSE_MOUNT.get(), MountEntity.createAttributes().build());
         event.put(ModEntities.SAKURA_WITCH.get(), SakurawitchEntity.createAttributes().build());
         event.put(ModEntities.TOY_BEAR.get(), ToyBearEntity.createAttributes().build());
     }
