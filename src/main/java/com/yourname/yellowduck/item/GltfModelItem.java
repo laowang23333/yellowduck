9d7d9b62baed35df261f1f6e727c96861240c29f
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-24 20:29:20 +0800

    Add files via upload

---
+++ src/main/java/com/yourname/yellowduck/item/GltfModelItem.java
@@ -0,0 +1,39 @@
+package com.yourname.yellowduck.item;
+
+import com.yourname.yellowduck.client.GltfItemRenderer;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+
+import java.util.function.Consumer;
+
+/** 使用 YellowDuck Native GLTF 直接渲染的 3D 物品。 */
+public final class GltfModelItem extends Item {
+    private final ResourceLocation modelLocation;
+    private final float visualScale;
+
+    public GltfModelItem(Properties properties, ResourceLocation modelLocation, float visualScale) {
+        super(properties);
+        this.modelLocation = modelLocation;
+        this.visualScale = visualScale;
+    }
+
+    public ResourceLocation getModelLocation() {
+        return modelLocation;
+    }
+
+    public float getVisualScale() {
+        return visualScale;
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return GltfItemRenderer.getInstance();
+            }
+        });
+    }
+}
