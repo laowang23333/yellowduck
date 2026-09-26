1c7280ad6b67c3a891aa56a96f9cdfa8fa7914eb
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-26 08:10:58 +0800

    Add files via upload

--- src/main/java/com/yourname/yellowduck/registry/ModCreativeTabs.java
+++ src/main/java/com/yourname/yellowduck/registry/ModCreativeTabs.java
@@ -17,41 +17,31 @@ public class ModCreativeTabs {
                     .title(Component.literal("小菜蛋"))
                     .icon(() -> new ItemStack(ModBlocks.JINYUANQUAN.get()))
                     .displayItems((params, output) -> {
-                        // 原有物品
                         output.accept(ModBlocks.BIG_CHEST_ITEM.get());
+                        output.accept(com.yourname.yellowduck.piratechest.DragonPalacePirateChestContent.DRAGON_PALACE_PIRATE_CHEST_ITEM.get());
+                        output.accept(com.yourname.yellowduck.distillation.DistillationContent.HEAT_CONDUCTING_COPPER_COMPONENT.get());
+                        output.accept(com.yourname.yellowduck.distillation.DistillationContent.HARDENED_GLASS.get());
+                        output.accept(com.yourname.yellowduck.distillation.DistillationContent.LARGE_DISTILLATION_PLATFORM.get());
                         output.accept(ModBlocks.JINYUANQUAN.get());
                         output.accept(ModBlocks.MEET_STONE_ITEM.get());
 
-                        // 副本管理定位方块
                         output.accept(ModBlocks.DUNGEON_ENTRANCE_MARKER_ITEM.get());
                         output.accept(ModBlocks.DUNGEON_BOSS_SPAWN_MARKER_ITEM.get());
-
-                        // 新增：两个方块
                         output.accept(ModBlocks.YIN_SHEN_XIANG_ITEM.get());
                         output.accept(ModBlocks.XIAOHUANGYA_ITEM.get());
-
-                        // 新增：两个唱片
                         output.accept(ModBlocks.XIAOQI_DISC.get());
                         output.accept(ModBlocks.YIDIYIDI_DISC.get());
-
-                        // 新增五张唱片
                         output.accept(ModBlocks.MUSIC_DISC_1.get());
                         output.accept(ModBlocks.MUSIC_DISC_2.get());
                         output.accept(ModBlocks.MUSIC_DISC_3.get());
                         output.accept(ModBlocks.MUSIC_DISC_4.get());
                         output.accept(ModBlocks.MUSIC_DISC_5.get());
-
-                        // 坐骑蛋：用于第一次绑定坐骑图鉴
                         output.accept(ModItems.GHOST_WOLF_MOUNT.get());
                         output.accept(ModItems.ALPACA_MOUNT.get());
                         output.accept(ModItems.RABBIT_MOUNT.get());
                         output.accept(ModItems.BAMBOO_HORSE_MOUNT.get());
-
-                        // 天狗驯服材料：没有天狗坐骑蛋。
                         output.accept(ModItems.RED_MOON_CRYSTAL.get());
                         output.accept(ModItems.HIGH_SADDLE.get());
-
-                        // Boss / 生物头颅展示方块
                         output.accept(ModBlocks.EARL_HEAD_ITEM.get());
                         output.accept(ModBlocks.SAKURA_HEAD_ITEM.get());
                         output.accept(ModBlocks.TOY_BEAR_HEAD_ITEM.get());
